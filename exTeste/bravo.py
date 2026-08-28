757c8cc3eb960efe6378bd204d3b8e5c6513100a
Pierina K <pierinak0@gmail.com>
2026-08-28 15:39:56 -0400

    problemas lógicos

---
+++ exTeste/bravo.py
@@ -0,0 +1,16 @@
+def solve_B():
+    linha1 = input()
+    linha2 = input()
+    linha3 = input()
+    linha4 = input()
+    linha5 = input()
+ 
+    print(linha1)
+    print("Bravo, bravo!")
+    print(linha2)
+    print("Bravo, bravo!")
+    print(linha3)
+    print(linha4)
+    print(linha5)
+    print(linha5)
+ 
