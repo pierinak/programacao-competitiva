b7eb057cd43acfa1a507948699ab974c0b9d37e5
Pierina K <pierinak0@gmail.com>
2026-08-28 16:01:52 -0400

    correção pontual

--- exTeste/bravo.py
+++ exTeste/bravo.py
@@ -4,13 +4,14 @@ def solve_B():
     linha3 = input()
     linha4 = input()
     linha5 = input()
- 
+
     print(linha1)
-    print("Bravo, bravo!")
+    print("Bravo,bravo!")
     print(linha2)
-    print("Bravo, bravo!")
+    print("Bravo,bravo!")
     print(linha3)
     print(linha4)
     print(linha5)
     print(linha5)
- 
+
+solve_B()
